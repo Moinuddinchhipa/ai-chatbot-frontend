d2244353fac2f8a3841471a078c1fcf92096ab8b
Moinuddin Chhipa <moinuddinchhipa042@gmail.com>
2025-07-17 11:14:11 +0530

    fix: update scripts to support cross-platform build using cross-env

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const sendMessage = async (message) => {
-  const response = await axios.post("http://localhost:8000/api/chatbot", { message });
+  const response = await axios.post("https://goklyn-ai-chatboat-project.onrender.com/api/chat", { message: input });
+
   return response.data.response;
 };
